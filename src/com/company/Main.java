39b7b063396c61d1a57291f3736d9d4490689be0
D J <dominik.jonke@outlook.com>
2021-04-30 18:56:07 +0200

    implement armstrong number calculator (without 0)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    for(int idx = 1; idx <= 1000; idx++) {
+	        DisplayArmstrongNumber(idx);
+        }
+    }
+
+    private static void DisplayArmstrongNumber(int arm) {
+        int one = 0, ten = 0, hundret = 0, dividerTen = 10, temp = arm;
+
+        one = arm % dividerTen;
+        arm /= dividerTen;
+        ten = arm % dividerTen;
+        hundret = arm / dividerTen;
+
+        one = one * one * one;
+        ten = ten * ten * ten;
+        hundret = hundret * hundret * hundret;
+
+        if(temp == (one + ten + hundret)) {
+            System.out.println(temp);
+        }
     }
 }
